3a6df7b5c0520f66b5571f71d832a02137123635
Deepak D <deepakdevaraj1507@gmail.com>
2025-07-16 10:47:10 +0530

    Add files via upload

--- Numbers.py
+++ Numbers.py
@@ -1,22 +1,17 @@
-#1. Write a function that takes two arguments, 145 and 'o', and uses the `format` function to return a formatted string. Print the result. Try to identify the representation used.
+#1.
 def formated(num,char):
     format_string="num:{0},char:{1}".format(num,char)
     return format_string
 string=formated(145,"o")
 print(string)
 
-#2. In a village, there is a circular pond with a radius of 84 meters. Calculate the area of the pond using the formula: Circle Area = π r^2. (Use the value 3.14 for π) 
-#   Bonus Question: If there is exactly 1.4 liters of water in a square meter, what is the total amount of water in the pond? 
-#   Print the answer without any decimal point in it. 
-#   Hint: Circle Area = π r^2 Water in the pond = Pond AreaWater per Square Meter 
-
+#2
 radius=84
 pi=3.14
 area=pi*(radius**2)
 print(area)
 
-#Bonus Question
-
+#2bonus
 radius=float(input("Enter ther radius: "))
 pi=3.14
 area=pi*(radius**2)
@@ -25,9 +20,7 @@ water=area*water_per_sqm
 
 print(round(water))
 
-#3. If you cross a 490meterlong street in 7 minutes, calculate your speed in meters per second. 
-#   Print the answer without any decimal point in it. Hint: Speed = Distance / Time 
-
+#3.
 distance=490
 time=7*60
 speed=distance/time
